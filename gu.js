e7dbae4f6cc2d00a55c52dc6b208518ceb182970
clux <analsandblaster@gmail.com>
2013-09-18 22:06:13 +0100

    rewrite gu to not have irc transport built in

--- gu.js
+++ gu.js
@@ -1,54 +1,36 @@
-var irc = require('irc');
-var path = require('path');
+var log = require('logule').init(module);
+var join = require('path').join;
 var hotReload = require('hot-reload');
+var Duplex = require('stream').Duplex;
 
-// Gu class
-// NB: extra listeners can be added to this.bot externally if you know what's what
-function Gu(server, name, opts, scriptPath, files) {
+function Gu(scriptPath, files, opts) {
   if (!(this instanceof Gu)) {
-    return new Gu(server, name, opts, scriptPath, files);
+    return new Gu(scriptPath, files, opts);
   }
-  this.bot = new irc.Client(server, name, opts);
+  Duplex.call(this, {objectMode: true});
+  opts = opts || {};
+  this.files = [];
+  for (var i = 0; i < (files || []).length; i += 1) {
+    var f = join(scriptPath, files[i]);
+    this.files.push(f);
+  }
 
-  var writer = this._write.bind(this);
-  // TODO: allow pm listens?
-  //this.bot.addListener('pm', function (nick, msg) {
-  //  writer(msg, null, nick);
-  //});
-  var chanReg = new RegExp('^' + name + '[\\s,\\:](.*)');
-  this.bot.addListener('message', function (from, to, msg) {
-    if (!chanReg.test(msg)) {
-      return; // message not for me
-    }
-    var content = msg.match(chanReg)[1].trim();
-    if (!content) {
-      return; // empty statement
-    }
-    writer(content, to, from);
-  });
-
-  this.bot.addListener('error', function () {}); // never care about errors
-
-  this._watchdir = scriptPath;
-  this._files = (files || []).map(function (f) {
-    return path.join(scriptPath, f);
-  });
-  this._reload();
-  // TODO: hotReload will uncache everything in the scripts directory
-  // thus clearing state kept in any of these, can this be remedied?
+  this.reload();
   hotReload.create(require)
     .watch(scriptPath)
     .uncache(scriptPath, true)
     .reload(scriptPath, true)
-    .afterReload(this._reload.bind(this))
+    .afterReload(this.reload.bind(this))
     .start();
 }
+Gu.prototype = Object.create(Duplex.prototype);
 
-Gu.prototype._reload = function () {
-  this._handlers = [];
+// this should not be used by implementations
+Gu.prototype.reload = function () {
+  this.handlers = [];
   // require all files on the passed in scripts list to reattach handlers
-  for (var i = 0; i < this._files.length; i += 1) {
-    var f = this._files[i];
+  for (var i = 0; i < this.files.length; i += 1) {
+    var f = this.files[i];
     try {
       var fn = require(f);
       if (!fn || 'function' !== typeof fn) {
@@ -56,35 +38,45 @@ Gu.prototype._reload = function () {
           f + " in scripts directory is not a function"
         );
       }
-      fn(this);
-      console.log('Loaded handlers from', f);
+      fn(this/*, this._state[f]*/);
+      log.info('Loaded handlers from', f);
     }
     catch (e) {
-      console.error('FAILED TO LOAD', f);
-      console.error(e.stack);
+      log.error('FAILED TO LOAD', f);
+      log.error(e.stack);
     }
   }
 };
 
-Gu.prototype._write = function (content, chan, from) {
-  for (var i = 0; i < this._handlers.length; i += 1) {
-    var handler = this._handlers[i];
-    if (handler.reg.test(content)) {
-      var match = content.match(handler.reg);
-      this.current = chan || from; // callback will .say to where it came from
-      handler.cb.apply(this, match.slice(1).concat(from)); // `from` sometimes needed
+// write objects with: {user, message} keys
+Gu.prototype._write = function (obj, encoding, cb) {
+  var msg = obj.message;
+  var user = obj.user;
+
+  for (var i = 0; i < this.handlers.length; i += 1) {
+    var handler = this.handlers[i];
+    if (handler.reg.test(msg)) {
+      log.trace(user + ':' + msg, '- matched:', handler.reg);
+      var match = msg.match(handler.reg);
+      var preparedSay = this.say.bind(this, user);
+      var args = match.slice(1).concat(preparedSay, obj.name || obj.user);
+
+      handler.cb.apply(this, args);
       break; // match found - job's done
     }
   }
+  cb();
 };
 
-// public API
+// needs to be here for some reason
+Gu.prototype._read = function () {};
+
 /**
  * say
  * Say msg in the channel the matching handler last fired
  */
-Gu.prototype.say = function (msg) {
-  this.bot.say(this.current, msg);
+Gu.prototype.say = function (user, msg) { // TODO: include name?
+  this.push({message: msg, user: user}); // can be _read later
 };
 
 /**
@@ -94,9 +86,8 @@ Gu.prototype.say = function (msg) {
  * Regular expression will be applied to a white space trimmed version of
  * messages addressed to the bots name (via chanReg).
  */
-Gu.prototype.on = function (reg, handler) {
-  this._handlers.push({reg: reg, cb: handler});
+Gu.prototype.handle = function (reg, handler) {
+  this.handlers.push({reg: reg, cb: handler});
 };
-// public property: `bot` - for `irc` module passthroughs
 
 module.exports = Gu;
