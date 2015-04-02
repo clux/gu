6e66a5bfb37d831572a83cc05661545fa1164642
clux <analsandblaster@gmail.com>
2015-04-02 12:03:31 +0100

    make tests run with nodeunit for coverage

--- test/handlers.test.js
+++ test/handlers.test.js
@@ -1,13 +1,18 @@
-var tap = require('tap')
-  , test = tap.test
+var Gu = require(process.env.GU_COV ? '../gu-cov' : '../')
+  , sulfur = require('sulfur')
   , fs = require('fs')
-  , Gu = require('../')
-  , join = require('path').join;
+  , join = require('path').join
+  ;
 
-test("example", function (t) {
-  var exBot = new Gu(join('..','example', 'scripts'), ['dice.js', 'weather.js'], {
-    noReload: true
+exports.example = function (t) {
+  t.expect(4);
+  var scriptsPath = join(process.cwd(), 'example', 'scripts');
+  var exBot = new Gu(scriptsPath, ['dice.js', 'weather.js'], {
+    noReload: true,
+    verbose: true
   });
+  sulfur.absorb(exBot.log, 'gu');
+
   var xs = [
     {user: '#chan:clux', name: 'clux', message: "roll d8"},
     {user: '#chan:aa', name: 'aa', message: 'weather oslo tuesday'}
@@ -21,21 +26,26 @@ test("example", function (t) {
   });
 
   setTimeout(function () {
-    t.ok(ys[0].message | 0, "dice roll a positive integer");
-    t.ok((ys[0].message | 0) <= 8, "dice roll lte 8");
-    t.equal(ys[1].message.slice(0, 28), 'weather for oslo on tuesday:', 'weather');
-    t.end();
+    t.equal(ys.length, 2, "got two messages");
+    if (ys.length === 2) {
+      t.ok(ys[0].message | 0, "dice roll a positive integer");
+      t.ok((ys[0].message | 0) <= 8, "dice roll lte 8");
+      t.equal(ys[1].message.slice(0, 28), 'weather for oslo on tuesday:', 'weather');
+    }
+    t.done();
   }, 10);
-});
+};
 
-test("hot reloading", function (t) {
+exports.hotReload = function (t) {
   var h1 = 'gu.handle(/hi/, function (say) { say("a"); });';
   var h2 = 'gu.handle(/hi/, function (say) { say("b"); });';
   var makeScript = function (h) {
     return 'module.exports = function (gu) { ' + h + ' };';
   };
-  fs.writeFileSync('./scripts/temp.js', makeScript(h1));
-  var rBot = new Gu('./scripts', ['temp.js']);
+  var scriptsPath = join(__dirname, 'scripts');
+  var tempFile = join(scriptsPath, 'temp.js');
+  fs.writeFileSync(tempFile, makeScript(h1));
+  var rBot = new Gu(scriptsPath, ['temp.js']);
 
   var ys = [];
   rBot.on('data', function (y) {
@@ -45,18 +55,18 @@ test("hot reloading", function (t) {
 
   setTimeout(function () {
     t.equal(ys[0].message, "a", "first message uses first handler");
-    fs.writeFileSync('./scripts/temp.js', makeScript(h2));
+    fs.writeFileSync(tempFile, makeScript(h2));
     setTimeout(function () {
       rBot.write({user: '#chan:clux', message: 'hi2'});
       setTimeout(function () {
         t.equal(ys[1].message, "b", "second message uses second handler");
-        fs.unwatchFile('./scripts/temp.js');
-        fs.unlinkSync('./scripts/temp.js');
-        t.end();
+        fs.unwatchFile(tempFile);
+        fs.unlinkSync(tempFile);
+        t.done();
         setTimeout(function () {
           process.exit(0); // can't easily kill reload watchers unfortunately
         }, 10);
       }, 10);
     }, 10);
   }, 10);
-});
+};
