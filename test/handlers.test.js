17751605521a0eb82947d84f9a088ac67d55b12b
clux <analsandblaster@gmail.com>
2015-04-05 14:09:03 +0100

    test channel is preserved when present

--- test/handlers.test.js
+++ test/handlers.test.js
@@ -5,7 +5,7 @@ var Gu = require(process.env.GU_COV ? '../gu-cov' : '../')
   ;
 
 exports.example = function (t) {
-  t.expect(4);
+  t.expect(8);
   var scriptsPath = join(process.cwd(), 'example', 'scripts');
   var exBot = new Gu(scriptsPath, ['dice.js', 'weather.js'], {
     noReload: true,
@@ -18,14 +18,18 @@ exports.example = function (t) {
     ys.push(y);
   });
 
-  exBot.write({user: 'clux', message: "roll d8"});
+  exBot.write({user: 'clux', channel: "#test", message: "roll d8"});
   exBot.write({user: 'aa', message: 'weather oslo tuesday'});
 
   setTimeout(function () {
     t.equal(ys.length, 2, "got two messages");
     if (ys.length === 2) {
       t.ok(ys[0].message | 0, "dice roll a positive integer");
+      t.equal(ys[0].user, 'clux', 'user preserved');
+      t.equal(ys[0].channel, '#test', 'channel preserved');
       t.ok((ys[0].message | 0) <= 8, "dice roll lte 8");
+      t.equal(ys[1].user, 'aa', 'user preserved in msg 2');
+      t.ok(!ys[1].channel, 'no channel in msg 2');
       t.equal(ys[1].message.slice(0, 28), 'weather for oslo on tuesday:', 'weather');
     }
     t.done();
